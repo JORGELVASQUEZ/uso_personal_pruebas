9a6ad330011c01d1ebc9f552304fa25763212cba
VasquezDelgado_Jorge <cejots71@gmail.com>
2026-02-26 14:49:33 -0600

    actualizacion

--- criptosistemas.js
+++
@@ -1,8 +0,0 @@
-//4- criptosistemas
-//4.1 -> introduccion al cifrado con clave publica
-//4.2 -> protocolo de diffie - hellman por intercambio de claves
-//4.3 -> cifrado simetrico y asimetrico
-//simetrico -> la misma clave para cifrar y descifrar, dificil de compartir la clave de forma segura, rapido en el procesamiento, 128 o 256 bites de logitud de clave, 
-//asimetrico -> una clave publica para cifrar y una clave privada para descifrar, facil de compartir la clave publica, pero la clave privada debe mantenerse segura, mas uso de recursos computacionales, 2048 o 4096 bites de longitud de clave, 
-
-
